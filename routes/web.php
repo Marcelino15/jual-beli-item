ff357987451963a4b222a9e8816d41ef76a4640a
Endra Setiawan <setiaendra18@gmail.com>
2021-01-21 09:22:49 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/','HomeController@index');
 
 Auth::routes();
@@ -30,21 +31,8 @@ Route::get('/keranjang-update/{id}', 'HomeController@updateBarang');
 Route::get('/tampil-keranjang/', 'HomeController@viewCart');
 
 
-Route::get('/admin', 'AdminController@index')->name('admin');
-Route::get('/admin/daftar_admin','AdminController@index_admin')->name('list-admin');
-Route::get('/admin/admin_tambah','AdminController@create_admin')->name('admin-create');
-Route::post('/admin/admin_store','AdminController@store_admin')->name('admin-store');
-Route::get('/admin/admin_edit/{id}','AdminController@edit_admin')->name('admin-edit');
-Route::put('/admin/admin_update/{id}','AdminController@update_admin')->name('admin-update');
-Route::delete('/admin/admin_delete/{id}','AdminController@destroy_admin')->name('admin-delete');
-
-Route::get('/admin/pembeli','AdminController@index_pembeli')->name('list-pembeli');
-Route::get('/admin/pembeli_tambah','AdminController@create_pembeli')->name('pembeli-create');
-Route::post('/admin/pembeli_store','AdminController@store_pembeli')->name('pembeli-store');
-Route::get('/admin/pembeli_edit/{id}', 'AdminController@edit_pembeli')->name('pembeli-edit');
-Route::put('/admin/pembeli_update/{id}','AdminController@update_pembeli')->name('pembeli-update');
-Route::delete('/admin/pembeli_delete/{id}','AdminController@destroy_pembeli')->name('pembeli-delete');
-
+Route::get('/admin', 'AdminController@index')
+        ->name('admin');
 Route::get('/admin/penjual','AdminController@index_penjual')->name('list-penjual');
 Route::get('admin/penjual_tambah','AdminController@create_penjual')->name('penjual-create');
 Route::post('admin/penjual_store','AdminController@store_penjual')->name('penjual-store');
